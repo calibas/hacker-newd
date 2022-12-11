24e9b10508aaba641610dda6506752376db97836
calibas <wharader@gmail.com>
2022-12-11 12:08:09 -0800

    sort options, numpy corrcoef, mutex, etc

--- hacker-newd.py
+++ hacker-newd.py
@@ -1,6 +1,12 @@
 from http.server import HTTPServer
-import hnserver, json, sched, time
-import threading, urllib.request, webbrowser
+import hnserver
+import json
+import sched
+import time
+import threading
+import urllib.request
+import webbrowser
+import numpy as np
 
 url_base = 'https://hacker-news.firebaseio.com/v0'
 item_base = url_base + '/item/'
@@ -9,42 +15,64 @@ top_stories = url_base + '/topstories'
 best_stories = url_base + '/beststories'
 new_stories = url_base + '/newstories'
 
+# Top, best or new stories
+hn_stories = top_stories
+
 # How many stories to display, max 500 (max 200 for best_stories)
 length = 100
 
-# How much should the algorithm favor recent stories.
+# How much should the algorithm penalize older stories.
 # Recommended values between 1.0 (none) and 2.0 (heavily favor recent)
-favor_recent = 1.2
+# HN appears to use a value around 1.5 (top) and 1.0 (best)
+favor_recent = 1.5
+
+# Order by heat, age, or default
+order_by = "heat"
+
+# Time limit for checking downranked/upranked stories
+rank_time_limit = 180
 
 # Port for HTTP server on localhost
 server_port = 3001
 
 item_list = []
 
+
 def main():
     global item_list
 
+    # Use a mutex to prevent separate threads from
+    # accessing item_list at the same time
+    mutex = threading.Lock()
+
     # Separate thread that pulls data from Firebase
     # and saves to item_list
-    u_thread = threading.Thread(target=update_thread)
+    u_thread = threading.Thread(target=update_thread, args=[mutex])
     u_thread.setDaemon(True)
     u_thread.start()
 
     # Sleep until item_list is full
-    while len(item_list) < length:
+    list_len = 0
+    while list_len < length:
         time.sleep(1)
+        mutex.acquire()
+        try:
+            list_len = len(item_list)
+        finally:
+            mutex.release()
 
-    # So we can pass item_list when using HTTPServer
+    # Use handler so we can pass item_list when using HTTPServer
     def handler(*args):
-        hnserver.HNServer(item_list, *args)
+        hnserver.HNServer(item_list, mutex, *args)
 
     web_server = HTTPServer(("localhost", server_port), handler)
     server_url = f"http://localhost:{server_port}"
     print(f"Server started {server_url}")
-    
-    # Open in default web browser 
+
+    # Open in default web browser
     webbrowser.open(server_url)
-    
+
+    # Run web server until keyboard interrupt
     try:
         web_server.serve_forever()
     except KeyboardInterrupt:
@@ -53,50 +81,95 @@ def main():
     web_server.server_close()
     print("Server stopped.")
 
-def update_thread():
-    update_list()
+
+def update_thread(mutex):
+    # Thread to run update_list() repeatedly using scheduler
+    update_list(mutex)
     s = sched.scheduler(time.time, time.sleep)
-    def repeat_update_list(sc): 
-        update_list()
+
+    def repeat_update_list(sc):
+        update_list(mutex)
         sc.enter(300, 1, repeat_update_list, (sc,))
     s.enter(300, 1, repeat_update_list, (s,))
     s.run()
 
-def update_list():
+
+def update_list(mutex):
     global item_list
+    temp_list = []
 
     print('Fetching data from API, just a moment...')
-    
-    top = fetch_list(f'{top_stories}.json')
-    item_list = []
+
+    top = fetch_list(f'{hn_stories}.json')
     for index, item_id in enumerate(top[0:length]):
         item = fetch_item(item_id)
         if item:
             item['age'] = (time.time() - item['time'])/60
-            item['heat'] = round(item['score']/pow(item['age'], favor_recent), 5)
+            
+            # Subtract 1 from score when it's less than an hour old,
+            # this is so heat wont be as high for very new stories
+            # time_adj = 0
+            # if item['age'] < 60:
+            #     time_adj = 1
+            # item['heat'] = round((item['score'] - time_adj) /
+            #                      pow(item['age'], favor_recent), 5)
+            
+            # Halve score when it's less than an hour old,
+            # this is so heat wont be as high for very new stories
+            time_adj = 1.0
+            # if item['age'] < 60:
+            #     time_adj = 0.5
+            # if item['age'] > 600:
+            #     time_adj = 1.5
+            item['heat'] = round((item['score'] * time_adj) /
+                                  pow(item['age'], favor_recent), 5)
+            
             item['pos'] = index + 1
-            item_list.append(item)
+            temp_list.append(item)
+            print(f" {round((item['pos']/length) * 100)}% complete", end='\r'),
         else:
             print(f"Error fetching item_id {item_id}")
+    print('')
 
-    item_list = sorted(item_list, key=lambda d: d['heat'], reverse=True)
+    temp_list = sorted(temp_list, key=lambda d: d['heat'], reverse=True)
     # print('')
-    # for index, item in enumerate(item_list):
+    # for index, item in enumerate(temp_list):
     #     print(
     #         f"{index + 1} ({item['pos']}): ({item['heat']}) - {item['title']} [{item['url']}] {item['id']}")
     print('')
-    print('Downranked:')
-    for index, item in enumerate(item_list):
-        if item['age'] < 120 and item['pos'] - index > 10:
-            print(
-                f"{index + 1} ({item['pos']}): ({item['heat']}) - {item['title']} [{item['url']}] {item['id']}")
-    print('')
-    print('Upranked:')
-    for index, item in enumerate(item_list):
-        if item['age'] < 120 and  index - item['pos'] > 10:
-            print(
-                f"{index + 1} ({item['pos']}): ({item['heat']}) - {item['title']} [{item['url']}] {item['id']}")
-    print('')
+    if hn_stories != new_stories:
+        print('Downranked:')
+        for index, item in enumerate(temp_list):
+            if item['age'] < rank_time_limit and item['pos'] - index > 10:
+                print(
+                    f"{index + 1} ({item['pos']}): ({item['heat']}) - {item['title']} [{item['url']}] {item['id']}")
+        print('')
+        print('Upranked:')
+        for index, item in enumerate(temp_list):
+            if item['age'] < rank_time_limit and index - item['pos'] > 10:
+                print(
+                    f"{index + 1} ({item['pos']}): ({item['heat']}) - {item['title']} [{item['url']}] {item['id']}")
+        print('')
+
+    if order_by == "age":
+        temp_list = sorted(temp_list, key=lambda d: d['age'])
+    if order_by == "default":
+        temp_list = sorted(temp_list, key=lambda d: d['pos'])
+
+    x = np.arange(1, len(temp_list) + 1)
+    y = np.array([])
+    for item in temp_list:
+        y = np.append(y, item['pos'])
+
+    corr = np.corrcoef(x, y)
+    print(f"r: {round(corr[0][1], 5)}")
+    
+    mutex.acquire()
+    try:
+        item_list = temp_list
+    finally:
+        mutex.release()
+
 
 def fetch_list(url):
     json_res = []
--- hnserver.py
+++ hnserver.py
@@ -1,54 +1,60 @@
 from http.server import BaseHTTPRequestHandler
 
 class HNServer(BaseHTTPRequestHandler):
-    def __init__(self, item_list, *args):
+    def __init__(self, item_list, mutex, *args):
         self.item_list = item_list
+        self.mutex = mutex
         BaseHTTPRequestHandler.__init__(self, *args)
 
     def do_GET(self):
         if self.path == "/" or self.path == "/index.html":
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
+            self.mutex.acquire()
+            try:
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
 
-            header = """
-                <!DOCTYPE html>
-                <html><head>
-                <meta charset='utf-8'>
-                <title>Hacker Newd</title>
-                <link rel='stylesheet' type='text/css' href='style.css'>
-                </head>
-                """
-            self.html_output(header)
+                header = """
+                    <!DOCTYPE html>
+                    <html><head>
+                    <meta charset='utf-8'>
+                    <title>Hacker Newd</title>
+                    <link rel='stylesheet' type='text/css' href='style.css'>
+                    </head>
+                    """
+                self.html_output(header)
 
-            self.html_output("""
-                <body>
-                <ol>
-                """)
-            for index, item in enumerate(self.item_list):
-                age = "0m"
-                if item['age'] >= 60:
-                    age = f"{round(item['age']/60)}h"
-                else:
-                    age = f"{round(item['age'])}m"
-                self.html_output(
-                    f"""
-                        <li class="item">
-                            <div class="index">{index + 1}</div>
-                            <div class="item-info">
-                                <div class="pos">{item['pos']}</div>
-                                <div class="heat">{round(item['heat'], 4)}</div>
-                                <div class="age">{age}</div>
-                            </div>
-                            <div class="link"><a href='{item['url']}'>{item['title']}</a></div> 
-                            <div class="comments">[<a href='{get_hn_url(item['id'])}'>comments</a>]</div>
-                        </li>
+                self.html_output("""
+                    <body>
+                    <ol>
                     """)
-            self.html_output("""
-                </ol>
-                </body>
-                </html>
-                """)
+                for index, item in enumerate(self.item_list):
+                    age = "0m"
+                    if item['age'] >= 60:
+                        age = f"{round(item['age']/60)}h"
+                    else:
+                        age = f"{round(item['age'])}m"
+                    pos_color = get_pos_color(item['pos'], index + 1)
+                    self.html_output(
+                        f"""
+                            <li class="item">
+                                <div class="index">{index + 1}</div>
+                                <div class="item-info">
+                                    <div class="pos" style="background-color:{pos_color}">{item['pos']}</div>
+                                    <div class="heat">{round(item['heat'], 4)}</div>
+                                    <div class="age">{age}</div>
+                                </div>
+                                <div class="link"><a href='{item['url']}'>{item['title']}</a></div> 
+                                <div class="comments">[<a href='{get_hn_url(item['id'])}'>comments</a>]</div>
+                            </li>
+                        """)
+                self.html_output("""
+                    </ol>
+                    </body>
+                    </html>
+                    """)
+            finally:
+                self.mutex.release()
 
         elif self.path == "/style.css":
             with open("style.css", 'rb') as file:
@@ -62,11 +68,27 @@ class HNServer(BaseHTTPRequestHandler):
             self.send_response(404)
             self.end_headers()
 
-    def update(self, new_list):
-        self.item_list = new_list
-
     def html_output(self, html_txt):
         self.wfile.write(bytes(html_txt, "utf-8"))
 
+
 def get_hn_url(item_id):
     return f"https://news.ycombinator.com/item?id={item_id}"
+
+def get_pos_color(item_pos, index):
+    pos_color = "#fff"
+    if item_pos - index > 10:
+        pos_color = "#ffe4e4"
+    if item_pos - index > 20:
+        pos_color = "#ffc3c3"
+    if item_pos - index > 50:
+        pos_color = "#ff9595"
+    if index - item_pos > 10:
+        pos_color = "#d9fdcf"
+    if index - item_pos > 20:
+        pos_color = "#9effa4"
+    if index - item_pos > 30:
+        pos_color = "#4fff5a"
+    
+    return pos_color
+    
